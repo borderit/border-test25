279fdd6034ddbb1f364dccc402c00a880ef890b5
Stephen Stanton <steve@border-it.com.au>
2016-01-03 18:06:08 +1100

    Initial Commit

---
+++ test25Tests/test25Tests.swift
@@ -0,0 +1,36 @@
+//
+//  test25Tests.swift
+//  test25Tests
+//
+//  Created by Steve Stanton on 3/01/2016.
+//  Copyright © 2016 Steve Stanton. All rights reserved.
+//
+
+import XCTest
+@testable import test25
+
+class test25Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
